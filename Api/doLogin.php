9057027a37d07b33358519905c386fc4c227d635
kelvinice <kelvinices@gmail.com>
2018-11-14 19:17:26 +0700

    bikin di lab

---
+++ Api/doLogin.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: prk
+ * Date: 14/11/2018
+ * Time: 16:51
+ */
+
---
+++ Script/indexStyleController.js
@@ -0,0 +1,15 @@
+$(function () {
+    $("#foot-bar").hover(function () {
+        $("#foot-bar").animate({bottom: "0%"}, "fast");
+        console.log("in");
+
+    },function () {
+        // console.log("out");
+        // $("#foot-bar").animate({bottom: "10%"}, "fast");
+    });
+
+    $("#stop-div").click(function () {
+        $("#foot-bar").animate({bottom: "-10%"}, "fast");
+    });
+});
+
